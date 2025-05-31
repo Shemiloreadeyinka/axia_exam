f14e38916a7a0e1d134d4aa17c7c7c75f7f98fb2
unknown <adeyinkasemilore@gmail.com>
2025-05-31 10:43:43 +0100

    initial commit

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,24 +1,24 @@
 const  bcrypt=require('bcrypt')
-const user= require('../models/user')
+const User= require('../models/user')
 
 
-exports.registerUser=async (res,req) => {
+exports.registerUser=async (req,res) => {
     try {
-        const {name,email,password,hobbies}=req.body
-        const userExists= await user.findOne({email})
+        const {name,email,password,isAdmin,hobbies}=req.body
+        const userExists= await User.findOne({email})
         if(userExists){ return res.status(400).json({message:"User already exists"}) }
         const hashedPassword=await bcrypt.hash(password,10)
-        const user=await user.create({name,email,password:hashedPassword,hobbies})
+        const user=await User.create({name,email,password:hashedPassword,isAdmin,hobbies})
         res.status(201).json({message:"User created successfully",user})}catch(error ){
-            res.status(500).json({message:error.message})
+            res.json({message:error.message})
         }
     
 }
 
-exports.loginUser=async(res,req)=>{
+exports.loginUser=async(req,res)=>{
     try {
         const {email,password}=req.body
-        const user=await user.findOne({email})
+        const user=await User.findOne({email})
         if(!user){ return res.status(400).json({message:"Invalid email or password"})
             }
         const isValidPassword=await bcrypt.compare(password,user.password)
@@ -30,9 +30,9 @@ exports.loginUser=async(res,req)=>{
         }
 }
 
-exports.getAllUsers= async (rreq,res) => {
+exports.getAllUsers= async (req,res) => {
     try {
-        const users=await user.find()
+        const users=await User.find()
         res.status(200).json({users})
         }catch(error){
             res.status(500).json({message:error.message})
@@ -43,7 +43,7 @@ exports.updateUser= async (req,res) =>  {
     try {
         const {id}=req.params
         const update=req.body
-        const updatedUser=await user.findByIdAndUpdate(id,update,{new:true})
+        const updatedUser=await User.findByIdAndUpdate(id,update,{new:true})
 
         res.status(200).json({user})}catch(error){
             res.status(500).json({message:error.message})
@@ -53,7 +53,7 @@ exports.updateUser= async (req,res) =>  {
 exports.deleteUser= async (req,res) =>{
     try {
         const {id}=req.params
-        const user=await user.findByIdAndDelete(id)
+        const user=await User.findByIdAndDelete(id)
         res.json({message:'user deleted'})
     }catch(error){
         res.status(500).json({message:error.message})
--- models/user.js
+++ models/user.js
@@ -4,10 +4,10 @@ const userSchema= new mongoose.Schema({
     name:{type:String, required:true},
     email:{type:String, required:true, unique:true},
     password:{type:String, required:true},
-    isAdmin:{type:String, required:true},
-    hobbies:{type:String , default:[]}
+    isAdmin:{type:Boolean, required:true},
+    hobbies:{type:[String] , default:[]}
 }
 , {timestamps:true})
 
-const user=mongoose.model('user',userSchema)
-module.exports=user;
+const User=mongoose.model('User',userSchema)
+module.exports=User;
